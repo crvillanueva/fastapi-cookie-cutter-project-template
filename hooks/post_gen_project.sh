79e3855b11577a2d2486d98e4160d5cafa2bbebf
crvillanueva <Cristobal>
2022-06-26 04:30:42 -0400

    agregar shebang para scripts

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -1,2 +1,4 @@
-poetry add -D black mypy autoflake isort python-dotenv
-poetry add fastapi[all]
+#!/bin/bash
+poetry init --author "Cristobal Villanueva" --no-interaction
+poetry add fastapi sqlalchemy
+poetry add -D black isort autoflake python-dotenv pytest mypy
---
+++ hooks/pre_gen_project.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+echo "Running pre_gen_project.sh"
