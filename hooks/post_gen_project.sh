5be65de64f795002a5fa4790192a1a748001c2f3
crvillanueva <Cristobal>
2022-08-12 18:09:57 -0400

    corregir prompt post gen script

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -2,13 +2,13 @@
 
 # Script to create remote repository and install dependencies with poetry and testing DB
 
-read -p "Enter the name of the remote repository: " PROYECT_DESCRIPTION
+read -p "Enter proyect description: " PROYECT_DESCRIPTION
 
 # create remote repository
 gh repo create --private --remote origin --source=. --description $PROYECT_DESCRIPTION
 
 # install dependencies
-poetry init \ 
+poetry init \
     --author "Cristobal Villanueva <cristobal.villanueva@geovalidata.com>" \
     --description $PROYECT_DESCRIPTION \
     --no-interaction
@@ -16,4 +16,4 @@ poetry add fastapi sqlalchemy
 poetry add -D mypy black isort autoflake python-dotenv pytest
 
 # install testing DB
-tch db.sqlite3 --batch db.sqlite3
+sqlite3 -batch db.sqlite3 "CREATE TABLE test (id INTEGER PRIMARY KEY, f TEXT);"
