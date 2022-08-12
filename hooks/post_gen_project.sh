3b6f46f5677d9992160d1d15f9b268faff3dedf0
crvillanueva <Cristobal>
2022-08-12 17:54:44 -0400

    agregar creacion de remote repo con GitHub CLI

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -1,4 +1,16 @@
-#!/bin/bash
-poetry init --author "Cristobal Villanueva" --no-interaction
+#!/usr/bin/env sh
+
+# Script to create remote repository and install dependencies with poetry
+
+read -p "Enter the name of the remote repository: " PROYECT_DESCRIPTION
+
+# create remote repository
+gh repo create --private --remote origin --source=. --description $PROYECT_DESCRIPTION
+
+# install dependencies
+poetry init \ 
+    --author "Cristobal Villanueva <cristobal.villanueva@geovalidata.com>" \
+    --description $PROYECT_DESCRIPTION \
+    --no-interaction
 poetry add fastapi sqlalchemy
-poetry add -D black isort autoflake python-dotenv pytest mypy
+poetry add -D mypy black isort autoflake python-dotenv pytest
