e9b9e739b421488c4d98147ca9139fbca4cf022d
Cristobal Villanueva <cristobaljvp@gmail.com>
2023-05-10 22:53:42 -0400

    feat+fix: paso a python3.11, corregir escape en justfile y permitir crear repo en organizacion de trabajo

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -8,15 +8,25 @@ read -p "Enter proyect description: " PROYECT_DESCRIPTION
 git init
 
 # create remote repository
-gh repo create --private --remote origin --source=. --description "$PROYECT_DESCRIPTION"
+if [[ {{ cookiecutter.work }} = y ]]; then
+  repo_name="geovalidata-spa/{{ cookiecutter.project_name }}"
+else
+  repo_name={{ cookiecutter.project_name }}
+fi
+
+gh repo create "$repo_name" --private --remote origin --source=. --description "$PROYECT_DESCRIPTION"
+
+git add .
+git commit -m "inicio proyecto $date"
+git push -u origin main
 
 # install dependencies
-/usr/bin/python3 -m venv .venv
+/usr/bin/python3.{{ cookiecutter.python_version }} -m venv .venv
 poetry init --quiet --no-interaction \
-    --author "Cristobal Villanueva <cristobal.villanueva@geovalidata.com>" \
+    --author "Cristobal Villanueva <cristobaljvp@gmail.com>" \
     --description "$PROYECT_DESCRIPTION"
-poetry add fastapi sqlalchemy uvicorn gunicorn
-poetry add -D mypy black isort autoflake python-dotenv pytest
+poetry add fastapi sqlalchemy uvicorn gunicorn python-dotenv 
+poetry add -D mypy black isort autoflake pytest ipdb
 
 # install testing DB
 sqlite3 -batch db.sqlite3 "CREATE TABLE test (id INTEGER PRIMARY KEY, f TEXT);"
